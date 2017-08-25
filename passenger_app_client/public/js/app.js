55d1cbbe88eab84933b377765a821814ea8d5172
Amanda <mrs.acapella@gmail.com>
2017-08-25 15:21:38 -0700

    angular up and running, express installed, users table in rails created

---
+++ passenger_app_client/public/js/app.js
@@ -0,0 +1,12 @@
+console.log("frontend passenger app.js");
+
+
+
+var app = angular.module('passengerRegistration', []);
+
+app.controller('mainController', ['$http', function($http){
+   this.message = "angular functioning!!";
+
+
+
+}]);
---
+++ passenger_app_client/public/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app     = express();
+
+var port = 4040;
+
+app.use(express.static('../public'));
+
+app.listen(port, function(){
+    console.log("======================");
+    console.log("Running on port: " + port);
+    console.log("Tell me all the things...");
+    console.log("======================");
+});
